b71c0a633d3618d6e614dfe809f00461dab8a034
Ridwan Salmanu <ridwan.salmanu96@yahoo.com>
2022-06-29 10:07:10 +0100

    hide environment variable with "python-dotenv"

---
+++ settings.py
@@ -0,0 +1,7 @@
+import os
+from dotenv import load_dotenv
+
+DB_NAME = os.environ['DB_NAME']
+DB_USER = os.environ['DB_USER']
+TEST_DB_NAME = os.environ['TEST_DB_NAME']
+DB_PASSWORD = os.environ['DB_PASSWORD']
